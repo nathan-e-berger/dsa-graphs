05e28b746e6c2303b4e25cc0ee257b677adda907
Nathan Berger <nberger101@gmail.com>
2023-11-09 14:57:30 -0500

    added methods up to and including DFS

--- graph.js
+++ graph.js
@@ -16,32 +16,63 @@ class Graph {
   }
 
   /** add Node instance and add it to nodes property on graph. */
-  addNode(node) { }
+  addNode(node) {
+    this.nodes.add(node);
+  }
 
   /** add array of new Node instances and adds to them to nodes property. */
-  addNodes(nodeArray) { }
+  addNodes(nodeArray) {
+    for (let node of nodeArray) {
+      this.addNode(node);
+    }
+  }
 
   /** add edge between nodes n1,n2 */
-  addEdge(n1, n2) { }
+  addEdge(n1, n2) {
+    n1.adjacent.add(n2);
+    n2.adjacent.add(n1);
+  }
 
   /** remove edge between nodes n1,n2 */
-  removeEdge(n1, n2) { }
+  removeEdge(n1, n2) {
+    n1.adjacent.delete(n2);
+    n2.adjacent.delete(n1);
+  }
 
   /** remove node from graph:
    *
    * - remove it from nodes property of graph
    * - update any adjacency lists using that node
    */
-  removeNode(node) { }
+  removeNode(node) {
+    this.nodes.delete(node);
+
+    for (let n of this.nodes) {
+      if (n.adjacent.has(node)) {
+        this.removeEdge(n, node);
+      }
+    }
+  }
 
   /** traverse graph with DFS and returns array of Node values */
-  depthFirstSearch(start) { }
+  depthFirstSearch(start, seen = new Set([start])) {
+    let nodeValues = [];
+    nodeValues.push(start.value);
 
-  /** traverse graph with BDS and returns array of Node values */
+    for (let ads of start.adjacent) {
+      if (!seen.has(ads)) {
+        seen.add(ads);
+        return [...nodeValues, ...this.depthFirstSearch(ads, seen)];
+      }
+    }
+    return nodeValues;
+  }
+
+  /** traverse graph with BFS and returns array of Node values */
   breadthFirstSearch(start) { }
 
   /** find the distance of the shortest path from the start node to the end node */
   distanceOfShortestPath(start, end) { }
 }
 
-module.exports = { Graph, Node }
+module.exports = { Graph, Node };
